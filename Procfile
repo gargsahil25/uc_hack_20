e869ff16e600756da8fb3aee3981263c73cca7da
Sahil Garg <sahilgarg@urbanclap.com>
2020-04-05 16:12:53 +0530

    build on heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: python server.py runserver 0.0.0.0:80
+web: python server.py
 
--- server.py
+++ server.py
@@ -307,7 +307,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
  
 def test(HandlerClass = SimpleHTTPRequestHandler,
          ServerClass = http.server.HTTPServer):
-    http.server.test(HandlerClass, ServerClass)
+    http.server.test(HandlerClass, ServerClass, "HTTP/1.0", 80)
  
 if __name__ == '__main__':
     test()
